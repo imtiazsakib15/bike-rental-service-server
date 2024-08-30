ace3ddc1be3ecf65dcfd6553fa5ccbd6e81a9a3a
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-08-30 08:35:56 +0600

    booking interface created

---
+++ src/modules/booking/booking.interface.ts
@@ -0,0 +1,10 @@
+import { Types } from 'mongoose';
+
+export interface IBooking {
+  userId: Types.ObjectId;
+  bikeId: Types.ObjectId;
+  startTime: Date;
+  returnTime: Date | null;
+  totalCost: number;
+  isReturned?: boolean;
+}
