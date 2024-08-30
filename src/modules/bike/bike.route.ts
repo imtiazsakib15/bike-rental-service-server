4ff51bcff36f1d38af99fa69d6cc3431824533ab
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-08-30 22:28:07 +0600

    validateRequest middleware created for validating data

--- src/modules/bike/bike.route.ts
+++ src/modules/bike/bike.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { BikeControllers } from './bike.controller';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { BikeValidationSchemas } from './bike.validation';
 
 const router = Router();
 
-router.post('/', BikeControllers.create);
+router.post(
+  '/',
+  validateRequest(BikeValidationSchemas.createSchema),
+  BikeControllers.create,
+);
 
 export const BikeRoutes = router;
